b385b7b2127ceb42540b8dd335355c06a2876223
Hammak <saint3543jakuzi@gmail.com>
2020-12-14 21:29:28 +0200

    Added User class

---
+++ ZhPPPSecretSantaTGBot/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace ZhPPPSecretSantaTGBot
+{
+    [Serializable]
+    public class User
+    {
+        public int Id;
+        public string Username;
+        public string FirstName;
+        public string LastName;
+        public States State;
+
+        public string OfficialName;
+        public string Phone;
+        public string PostNumber;
+        public string FanOf;
+        public string Ban;
+
+        public int TargetId;
+
+        public User()
+        {
+        }
+
+        public User(int id, string username, string firstName, string lastName)
+        {
+            Id = id;
+            Username = username;
+            FirstName = firstName;
+            LastName = lastName;
+            State = States.NewUser;
+        }
+    }
+
+    public enum States
+    {
+        NewUser,
+        StageOffName,
+        StagePhone,
+        StagePost,
+        StageFan,
+        StageBan,
+        Registered,
+        TargetChosen,
+        TargetSended
+    }
+}
