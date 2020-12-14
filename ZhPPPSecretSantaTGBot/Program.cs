a2da31a6f04a2ec6a4597fab63a7d4e61806039b
Hammak <saint3543jakuzi@gmail.com>
2020-12-14 20:09:00 +0200

    Added logging to main program

--- ZhPPPSecretSantaTGBot/Program.cs
+++ ZhPPPSecretSantaTGBot/Program.cs
@@ -8,33 +8,37 @@ namespace ZhPPPSecretSantaTGBot
     class Program
     {
         static ITelegramBotClient botClient;
+        static Logger logger;
 
         static void Main()
         {
+            logger = new Logger();
+
             if (File.Exists("config\\access token.txt"))
             {
                 StreamReader sr = new StreamReader("config\\access token.txt");
                 string accessToken = sr.ReadLine();
                 botClient = new TelegramBotClient(accessToken);
+                logger.Log("Access token successfully acquired");
             }
             else
             {
-                Console.WriteLine(
-                    "Please create directory \"config\" and put there a file \"access token.txt\" with your bot access token");
+                logger.Log("Please create directory \"config\" and put there a file " +
+                           "\"access token.txt\" with your bot access token");
                 Console.ReadKey();
                 return;
             }
 
             var me = botClient.GetMeAsync().Result;
-            Console.WriteLine(
-                $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
-            );
+            logger.Log($"Connected successfully. Bot ID {me.Id}; named {me.FirstName}.");
 
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+
+            logger.Log("Ended execution by user command");
 
             botClient.StopReceiving();
         }
@@ -43,7 +47,8 @@ namespace ZhPPPSecretSantaTGBot
         {
             if (e.Message.Text != null)
             {
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+                logger.Log($"Received a text message in chat {e.Message.Chat.Id}|@{e.Message.From.Username}|{e.Message.From.FirstName} {e.Message.From.LastName}");
+                logger.Log(e.Message.Text);
 
                 await botClient.SendTextMessageAsync(
                     chatId: e.Message.Chat,
