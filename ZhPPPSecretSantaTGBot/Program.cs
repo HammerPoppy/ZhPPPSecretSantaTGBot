02a7cee0a06127febff5d336710d08ddfc86bee1
Hammak <saint3543jakuzi@gmail.com>
2020-12-15 00:02:22 +0200

    Some refactor

--- ZhPPPSecretSantaTGBot/Program.cs
+++ ZhPPPSecretSantaTGBot/Program.cs
@@ -7,50 +7,52 @@ namespace ZhPPPSecretSantaTGBot
 {
     class Program
     {
-        static ITelegramBotClient botClient;
-        static Logger logger;
+        static ITelegramBotClient BotClient;
+        static Logger Logger;
+        private static DBHandler DBHandler;
 
         static void Main()
         {
-            logger = new Logger();
+            Logger = new Logger();
+            DBHandler = new DBHandler(Logger);
 
             if (File.Exists("config\\access token.txt"))
             {
                 StreamReader sr = new StreamReader("config\\access token.txt");
                 string accessToken = sr.ReadLine();
-                botClient = new TelegramBotClient(accessToken);
-                logger.Log("Access token successfully acquired");
+                BotClient = new TelegramBotClient(accessToken);
+                Logger.Log("Access token successfully acquired");
             }
             else
             {
-                logger.Log("Please create directory \"config\" and put there a file " +
+                Logger.Log("Please create directory \"config\" and put there a file " +
                            "\"access token.txt\" with your bot access token");
                 Console.ReadKey();
                 return;
             }
 
-            var me = botClient.GetMeAsync().Result;
-            logger.Log($"Connected successfully. Bot ID {me.Id}; named {me.FirstName}.");
+            var me = BotClient.GetMeAsync().Result;
+            Logger.Log($"Connected successfully. Bot ID {me.Id}; named {me.FirstName}.");
 
-            botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving();
+            BotClient.OnMessage += Bot_OnMessage;
+            BotClient.StartReceiving();
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
-            logger.Log("Ended execution by user command");
+            Logger.Log("Ended execution by user command");
 
-            botClient.StopReceiving();
+            BotClient.StopReceiving();
         }
 
         static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
             if (e.Message.Text != null)
             {
-                logger.Log($"Received a text message in chat {e.Message.Chat.Id}|@{e.Message.From.Username}|{e.Message.From.FirstName} {e.Message.From.LastName}");
-                logger.Log(e.Message.Text);
+                Logger.Log($"Received a text message in chat {e.Message.Chat.Id}|@{e.Message.From.Username}|{e.Message.From.FirstName} {e.Message.From.LastName}");
+                Logger.Log(e.Message.Text);
 
-                await botClient.SendTextMessageAsync(
+                await BotClient.SendTextMessageAsync(
                     chatId: e.Message.Chat,
                     text: "You said:\n" + e.Message.Text
                 );
