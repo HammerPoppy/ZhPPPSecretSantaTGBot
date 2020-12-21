64360c9957c6c70d8c19dae7cd2d8eee093b44ee
Hammak <saint3543jakuzi@gmail.com>
2020-12-21 02:55:31 +0200

    SHT init

---
+++ ZhPPPSecretSantaSortingHatTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZhPPPSecretSantaSortingHatTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
